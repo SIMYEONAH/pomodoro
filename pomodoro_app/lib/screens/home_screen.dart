b49a5d5675533b06be02c35b6981d23825246cbc
SIMYEONAH <simya0528@naver.com>
2023-11-06 22:52:14 +0900

    Timer and Pause play Done!

--- pomodoro_app/lib/screens/home_screen.dart
+++ pomodoro_app/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,16 +10,45 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  bool isRunning = false;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds = totalSeconds - 1;
+    });
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    );
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       body: Column(
         children: [
           Flexible(
+            flex: 1,
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                "25:00",
+                "$totalSeconds",
                 style: TextStyle(
                   color: Theme.of(context).cardColor,
                   fontSize: 89,
@@ -30,11 +61,13 @@ class _HomeScreenState extends State<HomeScreen> {
             flex: 3,
             child: Center(
               child: IconButton(
-                iconSize: 120,
-                onPressed: () {},
+                iconSize: 100,
+                onPressed: isRunning ? onPausePressed : onStartPressed,
                 color: Theme.of(context).cardColor,
-                icon: const Icon(
-                  Icons.play_circle_fill_outlined,
+                icon: Icon(
+                  isRunning
+                      ? Icons.pause_circle_outline
+                      : Icons.play_circle_outlined,
                 ),
               ),
             ),
@@ -47,6 +80,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Container(
                     decoration: BoxDecoration(
                       color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(50),
                     ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -59,6 +93,9 @@ class _HomeScreenState extends State<HomeScreen> {
                             color:
                                 Theme.of(context).textTheme.displayLarge!.color,
                           ),
+                        ),
+                        const SizedBox(
+                          height: 15,
                         ),
                         Text(
                           "0",
